89de2d1df8b43dda510c0ded3668c385e07fe64c
ZeFurtado <lukhasfurtado9@gmail.com>
2026-03-12 23:20:34 -0300

    Primeiro teste

---
+++ Js01/js/script.js
@@ -0,0 +1,9 @@
+alert("Devil may cry");
+document.writeln("Melzinho");
+
+let mensagem = "Minha mensagem";
+let mensagem2 = "Outra mensagem";
+let meuPeso = 81.5;
+let minhaAltura = 1.84;
+let minhaIdade = 25;
+let ehDoador = false;
