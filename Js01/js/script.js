25a6455667d64dc8c26d09d3d0fe9024332a1cd7
ZeFurtado <lukhasfurtado9@gmail.com>
2026-03-18 20:59:10 -0300

    Sintaxe, Variáveis, Constantes, Tipos

--- Js01/js/script.js
+++ Js01/js/script.js
@@ -1,9 +1,18 @@
-alert("Devil may cry");
-document.writeln("Melzinho");
-
 let mensagem = "Minha mensagem";
 let mensagem2 = "Outra mensagem";
 let meuPeso = 81.5;
 let minhaAltura = 1.84;
 let minhaIdade = 25;
 let ehDoador = false;
+
+document.writeln(mensagem);
+
+document.writeln("<p> Mensagem: " + mensagem + "</p>");
+
+document.writeln("<p> Peso: " + meuPeso + "Kg </p>")
+
+document.writeln(`<p> Altura: ${minhaAltura} </p>` );
+
+document.writeln(`<p> Idade: ${minhaIdade} </p>`);
+
+document.writeln(`<p> Doador: ${ehDoador} </p>` );
