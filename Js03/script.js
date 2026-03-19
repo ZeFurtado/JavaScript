18d5a8937b52b7961d0067828ee22daab0231735
ZeFurtado <lukhasfurtado9@gmail.com>
2026-03-18 21:46:25 -0300

    Operadores e operações

---
+++ Js03/script.js
@@ -0,0 +1,31 @@
+let n1 = 10; //inteiro
+let n2 = 2; //inteiro
+let n3 = "2"; //string texto
+
+document.writeln(`N1 ${n1} <br>`);
+document.writeln(`N2 ${n2} <br>`);
+document.writeln(`N3 ${n3} <br>`);
+
+
+console.log(n1 + n2); //Adição
+console.log(n1 - n2); //Subtração
+console.log(n1 * n2); //Multiplicação
+console.log(n1 / n2); //Divisão
+console.log(n1 % n2); //Resto ou módulo
+
+console.log(n2 == n3) // igual a
+console.log (n2 === n3) // identico a
+console.log (n2 != n3) //diferente de
+console.log (n2 !== n3) //não é identico
+
+console.log(n1 > n2); //Maior que
+console.log(n1 < n2); //Menor que
+console.log(n1 >= n2); //Maior ou igual
+console.log(n1 <= n2); //Menor ou igual
+
+let a = true;
+let b = false;
+console.log(a && b) // E AND
+console.log(a || b) // OU OR
+console.log(!a) //NÃO NOT (retorna false)
+console.log(!a) //NÃO NOT (retorna true)
