9b0fcdb84d105790fe69768d97a8aa71f1394028
ZeFurtado <lukhasfurtado9@gmail.com>
2026-04-16 22:10:49 -0300

    Js04

---
+++ Js04/Script.js
@@ -0,0 +1,16 @@
+document.writeln("<h1> Arrays - Vetor ou Matriz")
+
+let produtos = ['Arroz', 'Feijão', 'Leite'];
+var codigos =  Array(10, 20, 30);
+let meses = ['Jan', 'Fev', 'Mar', 'Abr'];
+
+
+//Adicionar no final push = empurre
+produtos.push('Açúcar');
+codigos.push(40, 50, )
+meses.push('Mai', 'Jun', 'Ago', '07');
+
+// Remover do final pop = estoruar
+produtos.pop();
+codigos.pop();
+meses.pop();
