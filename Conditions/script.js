9690f6042f395e3dd429ca7d795527e5777eb32f
ZeFurtado <lukhasfurtado9@gmail.com>
2026-04-23 23:26:04 -0300

    Conditions

---
+++ Conditions/script.js
@@ -0,0 +1,8 @@
+var vel = 78;
+console.log(`A velocidade do seu carro é ${vel}Km/h`);
+
+if(vel > 60)
+    {
+        document.writeln(`Você ultrapassou a velocidade permitida: MULTADO`)
+    }
+document.writeln(`Dirija usando o cinto de segurança`);
