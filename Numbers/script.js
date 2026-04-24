3031fce2b7a4ade8859b74fb032c4a6435ef8262
ZeFurtado <lukhasfurtado9@gmail.com>
2026-04-23 23:26:37 -0300

    Numbers

---
+++ Numbers/script.js
@@ -0,0 +1,9 @@
+const num1 = document.getElementById("numero");
+const num2 = document.getElementById("outro_numero");
+
+const retangulo = [4, 16];
+var areaRetangulo;
+
+
+document.writeln(`Altura: ${retangulo[0]} Largura: ${retangulo[1]}` );
+document.writeln(`Área do retângulo = ${retangulo[1] * retangulo[0]}`);
