e510956e832d8b1d24305d366a038a136671fa07
ZeFurtado <lukhasfurtado9@gmail.com>
2026-04-26 20:21:25 -0300

    Ordenacao

--- Ordenacao/script.js
+++ Ordenacao/script.js
@@ -12,3 +12,42 @@ function buscarElemento(lista, alvo){
 }
 
 console.log(`O número 4 se encontra na posição ${buscarElemento(array1, 4)} do array`);
+
+
+const arrayOrdenado = [3,6,9,15,24,36,45,55,87];
+const alvo = 55;
+
+console.log(`[Busca binária] Alvo: ${alvo} Array: ${arrayOrdenado.toString()}`);
+
+function buscaBinaria(lista, alvo){
+    let inicio = 0;
+    let fim = lista.length - 1;
+    let meio = parseInt((inicio + fim) / 2);
+    count = 0;
+
+    console.log(`Inicio: ${lista[inicio]}`);
+    console.log(`Meio: ${lista[meio]}`);
+    console.log(`Fim: ${lista[fim]}`);
+    
+    while(inicio <= fim){
+        let meio = parseInt((inicio + fim) / 2);
+        let valor = lista[meio];
+
+        if(valor == alvo){
+            return meio;
+        }else if(alvo < valor){
+            fim = meio - 1;
+        }else if(alvo > valor){
+            inicio = meio + 1;
+        }
+    }
+    return `Não foi possível encontrar o número ${alvo} no array`;
+    
+}
+
+console.log(`Resultado: ${buscaBinaria(arrayOrdenado, alvo)}`);
+
+console.log(`[Ordenação por inserção] `)
+
+
+
