8716387308bf71de9471510af073c36b0ca9ded7
ZeFurtado <lukhasfurtado9@gmail.com>
2026-04-26 01:27:26 -0300

    Funções

--- Funcoes/script.js
+++ Funcoes/script.js
@@ -15,3 +15,37 @@ function dizerNome (nome, numVezes){
 let numVezes = 5;
 dizerNome(`Augusto`, numVezes);
 
+function quantidadeDeCaracteres(palavra)
+{
+    if(typeof(palavra) !== String)
+        {
+            throw new console.error("O tipo de objeto não é uma string");
+            
+        }
+}
+
+function handleinput()
+{
+    const data = document.getElementById("hectares");
+
+    let hectares = data.value;
+    
+    document.getElementById("output"). innerText = hectares * 10000;
+}
+
+var numeros = [0, 8, 9, 8]
+
+let result;
+factorial = function(number)
+    {
+        if(number == 0){
+            return 1;   
+        } else{
+            result = number * factorial(number - 1);
+            return result;
+        }
+    }
+
+const fatorial = (x) => factorial(x);
+
+console.log(fatorial(5));
