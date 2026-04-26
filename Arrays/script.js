65f4418b33084b2ae42da33b27a8736f4e353fc5
ZeFurtado <lukhasfurtado9@gmail.com>
2026-04-26 14:10:07 -0300

    Arrays

--- Arrays/script.js
+++ Arrays/script.js
@@ -25,6 +25,55 @@ function imprimeOsmar()
     osmar = "Jair";
 }
 
-console.log(`${imprimeOsmar()} + ${osmar}`);
+
+
+const num = [5, 8, 9, 7]
+
+function calcularMediaArray(nums){
+    let result = 0;
+    let count = 0;
+    nums.forEach(element => {
+        result = result + element;
+        count++;
+    })
+    return `Result ${result} -- Count${count}`;
+}
+
+console.log(calcularMediaArray(num))
+
+const matriz = [["", "", ""], ["","",""],["","X",""]];
+
+function encontraX(matriz){
+    for(i = 0; i < matriz.length; i++){
+        let count = 1;
+        for(x = 0; x < matriz[i].length; x++){
+            if(matriz[i][x] == "X"){
+                return [i + 1, x + 1];
+            }
+        }
+    }
+}
+
+console.log(encontraX(matriz));
+
+const matrizString = [["Bola", "Bala", "Imã"], ["Vela", "Velho", "Várzea"],["Chulé", "Café", "Arroz"]];
+
+function contaLetrasIguais(matriz, letra){
+    var count = 0;
+    for(i = 0; i < matriz.length; i++){
+        for(x = 0; x < matriz[i].length; x++){
+            for(const l of matriz[i][x]){
+                console.log(`${l}`);
+                if(l.toLowerCase() == letra){
+                    count++;
+                };
+            }
+        }
+    }
+    return count;
+}
+
+console.log(contaLetrasIguais(matrizString, "a"));
+
 
 
